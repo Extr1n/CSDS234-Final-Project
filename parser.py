22f4c15b00ca5264cda2946c6eb9ca1ef01c1ac6
Extr1n <aidan.bugayong@gmail.com>
2025-04-30 17:20:10 -0400

    init parser file

---
+++ parser.py
@@ -0,0 +1,17 @@
+# This file parses the txt file into a json file
+import json
+
+datafilename = "data/amazon-meta.txt"
+
+
+json = {}
+
+with open(datafilename) as fn
+    for line in fn:
+        string = line.strip()
+        if string != "":
+
+
+
+
+
