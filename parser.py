fc3e08624ea12a56e374449ce5d15a1d3a639dd6
Cole Kerkemyer <cmk265@case.edu>
2025-05-01 11:56:50 -0400

    oops
    if fields was on the outside on accident

--- parser.py
+++ parser.py
@@ -46,18 +46,17 @@ def parse_block(lines):
                 total = int(hdr.get('total', 0))
                 revs = []
                 for j in range(1, total+1):
-                    fields={}
-                    if fields:
-                        if(i+j<len(lines)):
-                            fields = re.split(r'\s+', lines[i+j].strip())
-                        r = {'date': fields[0]}
-                        for s in range(1,len(fields),2):
-                            kk,vv = fields[s], fields[s+1]
-                            if vv != "":
-                                r[kk] = int(vv) if kk in ('rating','votes','helpful') else vv
-                            else:
-                                r[kk] = vv
-                        revs.append(r)
+                    if(i+j<len(lines)):
+                        fields = re.split(r'\s+', lines[i+j].strip())
+                        if fields:
+                            r = {'date': fields[0]}
+                            for s in range(1,len(fields),2):
+                                kk,vv = fields[s], fields[s+1]
+                                if vv != "":
+                                    r[kk] = int(vv) if kk in ('rating','votes','helpful') else vv
+                                else:
+                                    r[kk] = vv
+                            revs.append(r)
                 record['reviews'] = revs
                 i += total + 1
 
